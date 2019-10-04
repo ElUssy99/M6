86b08facb5d8a08784af0d447c6599e22b22c83b
David Usón <xldaviduson@gmail.com>
2019-10-04 17:56:58 +0200

    Continuacion de la practica
    Lectura y escritura al archivo binario

--- UF1_UsonD/src/Pt3/ArchivoBinario.java
+++ UF1_UsonD/src/Pt3/ArchivoBinario.java
@@ -1,18 +1,20 @@
 package Pt3;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class ArchivoBinario {
-	
+
 	static int count = 0;
-	
+
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		
+
 		boolean continuar = true;
 		while (continuar) {
 			System.out.println("// ALMACENAMIENTO DE INFORMACION //");
@@ -20,29 +22,29 @@ public class ArchivoBinario {
 			System.out.println("2 - Mostrar");
 			System.out.println("3 - Copia de seguridad");
 			System.out.println("4 - Salir");
-			
+
 			System.out.println("Escoge una opcion entre 1 y 7 (8 para salir):");
 			int opcion = entrada.nextInt();
 			switch (opcion) {
-				case 1:
-					opcion1();
-					count+=1;
-					break;
-				case 2:
-					opcion2();
-					break;
-				case 3:
-					opcion3();
-					break;
-				case 4:
-					continuar = opcion4();
-					break;
-				default:
-					System.out.println("No has escogido ninguna de las opciones.");
+			case 1:
+				opcion1();
+				count += 1;
+				break;
+			case 2:
+				opcion2();
+				break;
+			case 3:
+				opcion3();
+				break;
+			case 4:
+				continuar = opcion4();
+				break;
+			default:
+				System.out.println("No has escogido ninguna de las opciones.");
 			}
 		}
 	}
-	
+
 	public static void opcion1() {
 		Scanner entrada = new Scanner(System.in);
 		FileOutputStream fos = null;
@@ -50,26 +52,32 @@ public class ArchivoBinario {
 		try {
 			fos = new FileOutputStream("..\\UF1_UsonD\\src\\becadades.dat");
 			salida = new DataOutputStream(fos);
-			System.out.print("Nombre y 1rApellido:");
-			String nombre_apellido = entrada.nextLine();
-			System.out.print("Sexo (H|M):");
-			String StrSexo = entrada.nextLine();
-			char sexo = StrSexo.charAt(0);
-			System.out.print("Edad:");
+			String nombre_apellido;
+//			do {
+//				
+//			} while (condition);
+			System.out.print("Nombre y 1rApellido: ");
+			nombre_apellido = entrada.nextLine();
+			System.out.print("Sexo (H|M): ");
+			char sexo = entrada.next().charAt(0);
+			System.out.print("Edad: ");
 			int edad = entrada.nextInt();
-			System.out.print("Numero de suspensos:");
+			System.out.print("Numero de suspensos: ");
 			int suspensos = entrada.nextInt();
-			System.out.print("Ingresos anuales:");
+			System.out.print("Residencia familiar (SI|NO): ");
+			entrada.nextLine();
+			String residencia = entrada.nextLine();
+			System.out.print("Ingresos anuales: ");
 			float ingresos = entrada.nextFloat();
+			System.out.println("");
 			
-			salida.writeBytes("Alumno " + count);
-//			salida.writeBytes();
+			salida.writeUTF(nombre_apellido);
+			salida.writeChar(sexo);
+			salida.writeInt(edad);
+			salida.writeInt(suspensos);
+			salida.writeUTF(residencia);
+			salida.writeFloat(ingresos);
 			
-//			while (num != -1) {
-//				salida.writeInt(num); // se escribe el número entero en el fichero
-//				System.out.print("Introduce número entero. -1 para	acabar: ");
-//				num = entrada.nextInt();
-//			}
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 		} catch (IOException e) {
@@ -85,15 +93,33 @@ public class ArchivoBinario {
 			}
 		}
 	}
-	
+
 	public static void opcion2() {
-		
+		FileInputStream fis = null;
+		DataInputStream entrada = null;
+		int num;
+		try {
+			fis = new FileInputStream("..\\UF1_UsonD\\src\\becadades.dat");
+			entrada = new DataInputStream(fis);
+			while (true) {
+				String nombre_apellido = entrada.readUTF();
+				char sexo = entrada.readChar();
+				int edad = entrada.readInt();
+				int suspensos = entrada.readInt();
+				String residencia = entrada.readUTF();
+				float ingresos = entrada.readFloat();
+				
+				System.out.println(nombre_apellido + " - " + sexo + " - " + edad + " - " + suspensos + " - " + residencia + " - " + ingresos);
+			}
+		}catch (Exception e) {
+			System.err.println("Mensaje: " + e.getMessage());
+		}
 	}
-	
+
 	public static void opcion3() {
-		
+
 	}
-	
+
 	public static boolean opcion4() {
 		return false;
 	}
